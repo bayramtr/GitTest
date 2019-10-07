0120a886352afded83f4a0e15bf49c41ade431f2
Bayram Yılmaz <bayram.abcd>
2019-10-07 23:20:24 +0300

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Bayram Yılmaz on 7.10.2019.
+//  Copyright © 2019 Bayram Yılmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
